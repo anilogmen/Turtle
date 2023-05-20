e4ac1d0ae50887213308f7c7399a1aeb79c02d9b
Anıl Öğmen <anilogmn37@gmail.com>
2023-05-20 20:30:29 +0300

    OtomatikRandomYerDeğiştirmeTamamlandı

---
+++ turtleOtomatikRandomYerDeğiştirme.py
@@ -0,0 +1,23 @@
+import random
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("light blue")
+drawing_board.title("Python Turtle")
+
+T = turtle.Turtle()
+T.speed(1)
+
+a = [-100, 200]
+b = [-100, 200]
+def otomatik_yer_degistir():
+    x = random.choice(a)
+    y = random.choice(b)
+
+    T.penup()
+    T.goto(x, y)
+    T.pendown()
+while True:
+    otomatik_yer_degistir()
+
+turtle.mainloop()
